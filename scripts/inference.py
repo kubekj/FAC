9c156b81cf29ae1d3a4e1d3ee21b5806be92bee1
kubekj <kuba.jedraszka@gmail.com>
2024-06-24 18:18:49 +0200

    Changed inference.py to not load the model each time

--- scripts/inference.py
+++ scripts/inference.py
@@ -15,7 +15,6 @@
 
 
 import argparse
-
 import cv2
 import numpy as np
 import torch
@@ -23,10 +22,11 @@ import pandas as pd
 import os
 import zipfile
 
+from tqdm import tqdm
 from model import build_model
 
 @torch.no_grad()
-def inference(cfg, weight, img):
+def inference(model, img, device):
     if img is None:
         img = np.random.randint(0, 255, size=(112, 112, 3), dtype=np.uint8)
     else:
@@ -36,65 +36,62 @@ def inference(cfg, weight, img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = np.transpose(img, (2, 0, 1))
     img = torch.from_numpy(img).unsqueeze(0).float()
-    img.div_(255).sub_(0.5).div_(0.5)
+    img = img.div(255).sub(0.5).div(0.5).to(device)
 
-    model = build_model(cfg)
-    dict_checkpoint = torch.load(weight, map_location=torch.device('cpu'))
-    model.backbone.load_state_dict(dict_checkpoint["state_dict_backbone"])
-    model.fam.load_state_dict(dict_checkpoint["state_dict_fam"])
-    model.tss.load_state_dict(dict_checkpoint["state_dict_tss"])
-    model.om.load_state_dict(dict_checkpoint["state_dict_om"])
-
-    model.eval()
     output = model(img)
     return output
 
 class SwinFaceCfg:
     network = "swin_t"
-    fam_kernel_size=3
-    fam_in_chans=2112
-    fam_conv_shared=False
-    fam_conv_mode="split"
-    fam_channel_attention="CBAM"
-    fam_spatial_attention=None
-    fam_pooling="max"
-    fam_la_num_list=[2 for j in range(11)]
-    fam_feature="all"
+    fam_kernel_size = 3
+    fam_in_chans = 2112
+    fam_conv_shared = False
+    fam_conv_mode = "split"
+    fam_channel_attention = "CBAM"
+    fam_spatial_attention = None
+    fam_pooling = "max"
+    fam_la_num_list = [2 for j in range(11)]
+    fam_feature = "all"
     fam = "3x3_2112_F_s_C_N_max"
     embedding_size = 512
 
 def extract_zip(zip_file):
     img_dir = 'img_align_celeba'
-
     if not os.path.exists(img_dir):
         with zipfile.ZipFile(zip_file, 'r') as zip_ref:
             zip_ref.extractall()
         print(f"Extracted {zip_file}")
     else:
         print(f"{img_dir} already exists. Skipping extraction.")
-
     return img_dir
 
 def process_dataset(cfg, weight, img_dir, output_file):
-    results = []
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = build_model(cfg).to(device)
+    dict_checkpoint = torch.load(weight, map_location=device)
+    model.backbone.load_state_dict(dict_checkpoint["state_dict_backbone"])
+    model.fam.load_state_dict(dict_checkpoint["state_dict_fam"])
+    model.tss.load_state_dict(dict_checkpoint["state_dict_tss"])
+    model.om.load_state_dict(dict_checkpoint["state_dict_om"])
+    model.eval()
 
-    # List of attributes to exclude from printing
+    results = []
     exclude_attributes = ["Age", "Expression", "Recognition"]
 
-    for img_name in os.listdir(img_dir):
-        if img_name.endswith(".jpg"):
-            img_path = os.path.join(img_dir, img_name)
+    img_files = [img_name for img_name in os.listdir(img_dir) if img_name.endswith(".jpg")]
 
-            output = inference(cfg, weight, img_path)
-            result = {"image": img_name}
+    for img_name in tqdm(img_files, desc="Processing images"):
+        img_path = os.path.join(img_dir, img_name)
+        output = inference(model, img_path, device)
+        result = {"image": img_name}
 
-            for each in output.keys():
-                if each not in exclude_attributes:
-                    values = output[each][0].numpy()
-                    probabilities = np.exp(values) / np.sum(np.exp(values))
-                    result[each] = f'{probabilities[0]},{probabilities[1]}'
+        for each in output.keys():
+            if each not in exclude_attributes:
+                values = output[each][0].cpu().numpy()
+                probabilities = np.exp(values) / np.sum(np.exp(values))
+                result[each] = f'{probabilities[0]},{probabilities[1]}'
 
-            results.append(result)
+        results.append(result)
 
     df_results = pd.DataFrame(results)
     df_results.to_csv(output_file, index=False)
